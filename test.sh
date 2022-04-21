75f14faed210fcbd8294baf2e81e43fb5142cfd1
fsossai <federico.sossai@gmail.com>
2022-04-22 01:32:54 +0200

    Benchmark files are now part of the test

--- test.sh
+++ test.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-TEST_DIR=data
 CHECKER=./checker
 
 if [[ $# > 0 ]]; then
@@ -10,7 +9,7 @@ fi
 ERR=0
 TOTAL=0
 
-for f in $TEST_DIR/fail/*.json; do
+for f in data/fail/*.json; do
 	cat $f | $CHECKER 1> /dev/null 2> /dev/null
 	if [[ $? == 0 ]]; then
 		echo -e "\e[31mPASSED\e[0m : \e[1m$f\e[0m : $(cat $f)"
@@ -21,7 +20,7 @@ for f in $TEST_DIR/fail/*.json; do
 	TOTAL=$((TOTAL+1))
 done
 
-for f in $TEST_DIR/pass/*.json; do
+for f in data/pass/*.json data/benchmark/*; do
 	cat $f | $CHECKER 1> /dev/null 2> /dev/null
 	if [[ $? == 0 ]]; then
 		echo -e "\e[32mPASSED\e[0m : \e[1m$f\e[0m"
