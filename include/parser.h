b603f8f9dfb25a150df3ce66aacebc6c97fe41ed
fsossai <federico.sossai@gmail.com>
2024-01-18 19:46:27 -0600

    Added cmake

---
+++ include/parser.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include <string>
+
+#include "scanner.h"
+#include "ast.h"
+
+namespace json_parser {
+
+template<class T = File>
+class Parser {
+public:
+  Parser(const std::string& input)
+};
+
+}
